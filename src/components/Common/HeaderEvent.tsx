0418b883dbbe9d4fad49ee6bc3101da06ed19e20
RazaqFarhanR <aljawadfarhan@gmail.com>
2024-02-16 20:51:39 +0700

    fix: remove icon

--- src/components/Common/HeaderEvent.tsx
+++ src/components/Common/HeaderEvent.tsx
@@ -9,9 +9,10 @@ type Props = {}
 const HeaderEvent = (props: Props) => {
   return (
     <div className='bg-darkBlue w-auto py-3 flex justify-between px-8'>
+        <div></div>
         {/* <Image className='w-[42px] h-auto' src={KaypangIcon} alt='icon kaypang'/> */}
         <div className='flex items-center'>
-            <h1 className='font-lato text-white font-bold text-2xl'>PIALA BUPATI CUP MADIUN</h1>
+            <h1 className='font-lato text-white font-bold text-2xl'>APLIKASI PENILAIAN SILAT</h1>
         </div>
         <div className='flex space-x-2 w-auto'>
             {/* <Image className='w-auto h-[54px]' src={IpsiIcon} alt='icon ipsi'/> */}
